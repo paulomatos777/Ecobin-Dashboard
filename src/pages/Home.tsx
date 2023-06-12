f92629493ec21c004a94b0a6970a6ef4ed848723
Paulo Matos <paulomatos4554@gmail.com>
2023-06-11 21:55:08 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,80 +1,150 @@
-import React, { useEffect } from 'react';
 import './styles.css';
-import PHOTO from  './../assets/PHOTO.png'
-// import Group from  './../assets/Group.svg'
-// import Group2 from  './../assets/Group2.svg'
-// import PHOTO from  './../assets/PHOTO.png'
+import { Chart } from "react-google-charts";
+import profile from  './../assets/profile.jpeg'
+import { EcobinIcon } from '../icons/EcobinIcon';
+import { DashboardSectionIcon } from '../icons/DashboardSection';
+import { ManuntencaoSectionIcon } from '../icons/ManutencaoSectionIcon';
+import { RelatorioSectionIcon } from '../icons/RelatorioSectionIcon';
+import { NotificationIcon } from '../icons/NotificationIcon';
+import { MessageIcon } from '../icons/MessageIcons';
 
 const Home = () => {
-//   useEffect(() => {
-//     google.charts.load('current', {'packages': ['bar', 'corechart']});
-//     google.charts.setOnLoadCallback(drawCharts);
 
-//     function drawCharts() {
-//       var data1 = google.visualization.arrayToDataTable([
-//         ['2023', 'Lixo Comum', 'Lixo Molhado', 'Lixo Reciclável'],
-//         ['Norte', 1000, 400, 200],
-//         ['Sul', 1170, 460, 250],
-//         ['Oeste', 660, 1120, 300],
-//         ['Leste', 1030, 540, 350]
-//       ]);
+  //Bar Chart Graph Data
+   const data = [
+    ["Year", "Sales", "Expenses", "Profit"],
+    ["2014", 1000, 400, 200],
+    ["2015", 1170, 460, 250],
+    ["2016", 660, 1120, 300],
+    ["2017", 1030, 540, 350],
+  ];
 
-//       var options1 = {
-//         chart: {
-//           title: 'Kg de Lixo Descartado por Região',
-//           subtitle: 'Quantidade de lixo descartado por tipo e região',
-//         },
-//         colors: ['#225044', '#88AF95', '#D1E8D4']
-//       };
 
-//       var chart1 = new google.charts.Bar(document.getElementById('columnchart_material'));
-//       chart1.draw(data1, google.charts.Bar.convertOptions(options1));
+   const options = {
+    chart: {
+      title: "Lixo Tratado por Zonas de São Paulo",
+      subtitle: "",
+    },
+    colors:['#41AA90', '#85B6FF', '#562D1B'] 
+  };
 
-//       var data2 = google.visualization.arrayToDataTable([
-//         ['Task', 'Hours per Day'],
-//         ['Work', 11],
-//         ['Eat', 2],
-//         ['Commute', 2]
-//       ]);
 
-//       var options2 = {
-//         chart: {
-//           title: 'Kg de Lixo Descartado por Região',
-//           subtitle: 'Quantidade de lixo descartado por tipo e região',
-//         },
-//         colors: ['#225044', '#88AF95', '#D1E8D4']
-//       };
-
-//       var chart2 = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-//       chart2.draw(data2, options2);
-//     }
-//   }, []);
-
+  //Line Chart Graph Data
+const LineData =  [
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+  
+const LineOptions = {
+  title: "Company Performance",
+  curveType: "function",
+  legend: { position: "bottom" },
+  colors:['#41AA90', '#85B6FF', '#562D1B'] 
+};
 
 
   return (
-    <div>
-      <div className="square" id="one"></div>
-      <div className="square" id="two"></div>
-      <div className="square" id="three"></div>
-      <div className="square" id="four"></div>
-      <div className="square" id="five"></div>
+    <div className='home-container'>
+      <div className='first-session'>
+        <div className="column-sidebar">
+            <div className='rectangle-3'>
+              <div className='sidebar'>
+                <div className='ecobin-logo-wrapper'>
+                  <EcobinIcon/>
+                </div>
+                <div className='sidebar-categories'>
+                    <div className='menu-section'>
+                      <DashboardSectionIcon/>
+                      <div className='subcategoria-option'>  
+                          Visão Geral
+                      </div>
+                    </div>
+                    <div  className='menu-section'>
+                      <ManuntencaoSectionIcon/>
+                      <div className='subcategoria-option'>
+                          Rotina
+                      </div>
+                      <div className='subcategoria-option'>
+                          Chamados
+                      </div>
+                    </div>
+                    <div  className='menu-section'>
+                      <RelatorioSectionIcon/>
+                      <div className='subcategoria-option'>
+                          Financeiro
+                      </div>
+                      <div className='subcategoria-option'>
+                          Desempenho
+                      </div>
+                      <div className='subcategoria-option'>
+                          Empresa de Coleta
+                      </div>
+                      <div className='subcategoria-option'>
+                          Condomínios
+                      </div>
+                    </div>
+                </div>
+              </div>
 
-      <img src={PHOTO} alt="Profile Picture" className="profile" />
+            </div>
+        </div>
+        <div className="column-data">
+          <div className='data-first'>
+            <div className='status-wrapper'>
+              <div className='image-status'>
+                 <img src={profile} alt="status" className="profile"/>
+              </div>
+              <div className='name-section'>
+                <p className='name'>Paulo Fernando A. Matos Filho</p>
+                <p className='cargo'>Analista de Operações</p>
+              </div>
 
-      <input type="text" placeholder="Pesquisar" className="search" />
+            </div>
+            <div className='end-content'>
+                <input type="text" className='input' placeholder='Pesquisar'/>
+              <div className='top-icons'>
+                <NotificationIcon/>
+                <MessageIcon/>
+              </div>
+            </div>
+           
+            
+          </div>
+          <div className='data-second'>
+            <div className='rectangle-5'>
+                <Chart
+                  chartType="Bar"
+                  width="100%"
+                  height="100%"
+                  data={data}
+                  options={options}
+                />
+            </div>
+            <div className='rectangle-6'>
+               <Chart
+                chartType="LineChart"
+                width="100%"
+                height="100%"
+                data={LineData}
+                options={LineOptions}
+                />
+              </div>
+          </div>
+        </div>
+      </div>
 
-      <p className="user">Maria Leticia Fernandes</p>
-      <p className="cargo">Analista de operações</p>
+      
+      <div className='second-session'>
+        <div className='rectangle-4'>
+            <div className='wrapper-sheet'>
+              
+            </div>
+        </div>
+      </div>
 
-      <div className="status"></div>
-
-      <div id="columnchart_material" style={{ width: '500px', height: '350px' }}></div>
-
-      <div id="piechart_3d" style={{ width: '38%', height: '400px' }}></div>
-
-      <div className="chat"></div>
-      <div className="notification"></div>
     </div>
   );
 };
